5acd9330fd0e7539c9fdad7b3c4226a8ebb24cc5
feigningfigure <andrew.g.wallace@gmail.com>
2014-05-21 18:24:20 -0400

    migration change 2

--- db/migrate/20140521151511_remove_user_id_from_location_message_id_from_user_location_id_from_message.rb
+++ db/migrate/20140521151511_remove_user_id_from_location_message_id_from_user_location_id_from_message.rb
@@ -2,6 +2,6 @@ class RemoveUserIdFromLocationMessageIdFromUserLocationIdFromMessage < ActiveRec
   def change
     remove_column :locations, :user_id, :integer
     remove_column :messages, :location_id, :integer
-    # remove_column :users, :message_id, :integer
+    remove_column :users, :message_id, :integer
   end
 end
