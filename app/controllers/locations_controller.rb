508e606f5c180685405181a0139f994933d6282e
feigningfigure <andrew.g.wallace@gmail.com>
2014-05-21 21:37:26 -0400

    changed back no log-in, log-out option.  Avatars not showing

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,10 +8,10 @@ class LocationsController < ApplicationController
     @same_address_locations = Location.where({address: current_user.location.address})
     # render json: @same_address_locations
 
-    @nearby_as_selected_by_geocoder_locations =[]         #### TODO: This is expensive. Refactor someday.
+    @geocoded_nearby_locations =[]         #### TODO: This is expensive. Refactor someday.
         Location.all.each do |location|
           if location.distance_to(current_user) < 1
-            @nearby_as_selected_by_geocoder_locations << location
+            @geocoded_nearby_locations << location
           end
         end
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -28,7 +28,8 @@ end
     @current_user = User.authenticated?(email, password)
 
     if @current_user
-      @current_user.update({logged_in: true})
+      # @current_user.logged_in = true
+      # @current_user.save
       redirect_to 'messages/new'
     else
       render text: "Invalid email or password.  Please try again."
@@ -36,10 +37,11 @@ end
   end
 
   def destroy
-    # @current_user.update({logged_in: false})
-    # current_user.message.location.destroy
-    # current_user.message.destroy
-    # location.message.id = nil
+    # @current_user.logged_in = false
+    # # current_user.message.location.destroy
+    # # current_user.message.destroy
+    # # location.message.id = nil
+    # @current_user.save
     session[:user_id] = nil
 
     redirect_to login_url, :notice => "You are now logged out."
