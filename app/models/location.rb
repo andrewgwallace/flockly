8005a752b30a205bcceb5cf79ffb3cd7c0631550
feigningfigure <andrew.g.wallace@gmail.com>
2014-05-23 09:04:49 -0400

    removed superfluous files and added readme.md

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,7 +3,6 @@ class Location < ActiveRecord::Base
   belongs_to :message
   belongs_to :user
 
-  #attr_accessible :address, :latitude, :longitude
   geocoded_by :address
   reverse_geocoded_by :latitude, :longitude
   after_validation :geocode, :if => :address_changed?
@@ -24,16 +23,5 @@ class Location < ActiveRecord::Base
     user_location = user.location
     return Geocoder::Calculations.distance_between([user_location.get_latitude, user_location.get_longitude], [self.get_latitude, self.get_longitude])
   end
-
-  # def is_close_enough(distance = 1)
-  #   if distance_to() < distance
-  #     return true
-  #   else
-  #     return false
-  #   end
-  # end
-
-
-  # has_many :user
 
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -6,14 +6,5 @@ class Message < ActiveRecord::Base
   belongs_to :user
   has_one :location
 
-  # include WillPaginate
-
-  # has_many :user
-
-  # def distance_to(user)     #### MOVED TO LOCATION MODEL. PROBABALY SHOULD DELETE
-  #   # binding.pry
-  #   user_location = user.message.location
-  #   Geocoder::Calculations.distance_between([user_location.get_latitude, user_location.get_longitude], [self.location.get_latitude, self.location.get_longitude])
-  # end
 
 end
